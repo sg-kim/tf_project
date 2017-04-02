fda2e4a4795dbc838ff9492d06fdfb06f2495d18
sgkim <sgkim08@gmail.com>
2017-04-02 22:34:28 +0900

    Desktop Apr. 2, 2017

--- sung_kim_ML/Lab_11_CNN_MNIST_number_classification.py
+++ sung_kim_ML/Lab_11_CNN_MNIST_number_classification.py
@@ -38,21 +38,24 @@ x_for_conv = tf.reshape(x, [-1, 28, 28, 1], name='input_reshape')
 cudnn_on_gpu = True
 
 with tf.name_scope("conv_7x7_layer_1") as scope:
-    #W_conv_l1 = tf.Variable(tf.random_uniform([7, 7, 1, 64], -1.0, 1.0))
-    #b_conv_l1 = tf.Variable(tf.random_uniform([1, 64], -1.0, 1.0))
-    W_conv_l1 = tf.get_variable("W_conv_l1", shape=[7, 7, 1, 64], initializer=xavier_init(49, 3136))
-    b_conv_l1 = tf.get_variable("b_conv_l1", shape=[1, 64], initializer=xavier_init(64, 64))
+##    W_conv_l1 = tf.Variable(tf.random_uniform([7, 7, 1, 64], -1.0, 1.0))
+##    b_conv_l1 = tf.Variable(tf.random_uniform([1, 64], -1.0, 1.0))
+    W_conv_l1 = tf.Variable(tf.random_normal([7, 7, 1, 64]))
+    b_conv_l1 = tf.Variable(tf.random_normal([1, 64]))
+##    W_conv_l1 = tf.get_variable("W_conv_l1", shape=[7, 7, 1, 64], initializer=xavier_init(49, 3136))
+##    b_conv_l1 = tf.get_variable("b_conv_l1", shape=[1, 64], initializer=xavier_init(64, 64))
 
     conv_l1 = tf.nn.relu(tf.nn.conv2d(x_for_conv, W_conv_l1, strides=[1, 1, 1, 1],
                            use_cudnn_on_gpu=cudnn_on_gpu, padding='SAME') + b_conv_l1,
                          name='conv_7x7_layer_1')
 
 with tf.name_scope("conv_5x5_layer_2") as scope:
-    #W_conv_l2 = tf.Variable(tf.random_uniform([5, 5, 64, 64], -1.0, 1.0))
-    #b_conv_l2 = tf.Variable(tf.random_uniform([1, 64], -1.0, 1.0))
-    W_conv_l2 = tf.get_variable("W_conv_l2", shape=[5, 5, 64, 64], initializer=xavier_init(1600, 1600))
-    b_conv_l2 = tf.get_variable("b_conv_l2", shape=[1, 64], initializer=xavier_init(64, 64))
-
+##    W_conv_l2 = tf.Variable(tf.random_uniform([5, 5, 64, 64], -1.0, 1.0))
+##    b_conv_l2 = tf.Variable(tf.random_uniform([1, 64], -1.0, 1.0))
+    W_conv_l2 = tf.Variable(tf.random_normal([5, 5, 64, 64]))
+    b_conv_l2 = tf.Variable(tf.random_normal([1, 64]))    
+##    W_conv_l2 = tf.get_variable("W_conv_l2", shape=[5, 5, 64, 64], initializer=xavier_init(1600, 1600))
+##    b_conv_l2 = tf.get_variable("b_conv_l2", shape=[1, 64], initializer=xavier_init(64, 64))
 
     conv_l2 = tf.nn.relu(tf.nn.conv2d(conv_l1, W_conv_l2, strides=[1, 1, 1, 1],
                            use_cudnn_on_gpu=cudnn_on_gpu, padding='SAME') + b_conv_l2,
@@ -64,20 +67,24 @@ with tf.name_scope("max_pooling_2x2_layer_3") as scope:
                             name='max_pool_2x2_layer_3')
 
 with tf.name_scope("conv_7x7_layer_4") as scope:
-    #W_conv_l4 = tf.Variable(tf.random_uniform([7, 7, 64, 32], -1.0, 1.0))
-    #b_conv_l4 = tf.Variable(tf.random_uniform([1, 32], -1.0, 1.0))
-    W_conv_l4 = tf.get_variable("W_conv_l4", shape=[7, 7, 64, 32], initializer=xavier_init(3136, 1568))
-    b_conv_l4 = tf.get_variable("b_conv_l4", shape=[1, 32], initializer=xavier_init(32, 32))
+##    W_conv_l4 = tf.Variable(tf.random_uniform([7, 7, 64, 32], -1.0, 1.0))
+##    b_conv_l4 = tf.Variable(tf.random_uniform([1, 32], -1.0, 1.0))
+    W_conv_l4 = tf.Variable(tf.random_normal([7, 7, 64, 32]))
+    b_conv_l4 = tf.Variable(tf.random_normal([1, 32]))
+##    W_conv_l4 = tf.get_variable("W_conv_l4", shape=[7, 7, 64, 32], initializer=xavier_init(3136, 1568))
+##    b_conv_l4 = tf.get_variable("b_conv_l4", shape=[1, 32], initializer=xavier_init(32, 32))
 
     conv_l4 = tf.nn.relu(tf.nn.conv2d(mxp_l3, W_conv_l4, strides=[1, 1, 1, 1],
                            use_cudnn_on_gpu=cudnn_on_gpu, padding='SAME') + b_conv_l4,
                          name='conv_7x7_layer_4')
     
 with tf.name_scope("conv_5x5_layer_5") as scope:
-    #W_conv_l5 = tf.Variable(tf.random_uniform([5, 5, 32, 32], -1.0, 1.0))
-    #b_conv_l5 = tf.Variable(tf.random_uniform([1, 32], -1.0, 1.0))
-    W_conv_l5 = tf.get_variable("W_conv_l5", shape=[5, 5, 32, 32], initializer=xavier_init(800, 800))
-    b_conv_l5 = tf.get_variable("b_conv_l5", shape=[1, 32], initializer=xavier_init(32, 32))
+##    W_conv_l5 = tf.Variable(tf.random_uniform([5, 5, 32, 32], -1.0, 1.0))
+##    b_conv_l5 = tf.Variable(tf.random_uniform([1, 32], -1.0, 1.0))
+    W_conv_l5 = tf.Variable(tf.random_normal([5, 5, 32, 32]))
+    b_conv_l5 = tf.Variable(tf.random_normal([1, 32]))
+##    W_conv_l5 = tf.get_variable("W_conv_l5", shape=[5, 5, 32, 32], initializer=xavier_init(800, 800))
+##    b_conv_l5 = tf.get_variable("b_conv_l5", shape=[1, 32], initializer=xavier_init(32, 32))
 
     conv_l5 = tf.nn.relu(tf.nn.conv2d(conv_l4, W_conv_l5, strides=[1, 1, 1, 1],
                            use_cudnn_on_gpu=cudnn_on_gpu, padding='SAME') + b_conv_l5,
@@ -93,27 +100,30 @@ prob_keep = tf.placeholder(tf.float32)
 
 with tf.name_scope("full_connection_layer_7") as scope:
     #W_fc_l7 = tf.Variable(tf.random_uniform([1568, 1024], -1.0, 1.0))
-    #b_fc_l7 = tf.Variable(tf.random_uniform([1, 1024], -1.0, 1.0))
+##    b_fc_l7 = tf.Variable(tf.random_uniform([1, 1024], -1.0, 1.0))
+    b_fc_l7 = tf.Variable(tf.random_normal([1, 1024]))
     W_fc_l7 = tf.get_variable("W_fc_l7", shape=[1568, 1024], initializer=xavier_init(1568, 1024))
-    b_fc_l7 = tf.get_variable("b_fc_l7", shape=[1, 1024], initializer=xavier_init(1024, 1024))
+##    b_fc_l7 = tf.get_variable("b_fc_l7", shape=[1, 1024], initializer=xavier_init(1024, 1024))
 
     fc_l7 = tf.nn.relu(tf.matmul(mxp_l6_for_fc, W_fc_l7) + b_fc_l7, name='fully_conn_layer_7')
     fc_l7 = tf.nn.dropout(fc_l7, prob_keep, name='dropout_fc_layer7')
 
 with tf.name_scope("full_connection_layer_8") as scope:
     #W_fc_l8 = tf.Variable(tf.random_uniform([1024, 256], -1.0, 1.0))
-    #b_fc_l8 = tf.Variable(tf.random_uniform([1, 256], -1.0, 1.0))
+##    b_fc_l8 = tf.Variable(tf.random_uniform([1, 256], -1.0, 1.0))
+    b_fc_l8 = tf.Variable(tf.random_normal([1, 256]))
     W_fc_l8 = tf.get_variable("W_fc_l8", shape=[1024, 256], initializer=xavier_init(1024, 256))
-    b_fc_l8 = tf.get_variable("b_fc_l8", shape=[1, 256], initializer=xavier_init(256, 256))
+##    b_fc_l8 = tf.get_variable("b_fc_l8", shape=[1, 256], initializer=xavier_init(256, 256))
 
     fc_l8 = tf.nn.relu(tf.matmul(fc_l7, W_fc_l8) + b_fc_l8, name='fully_conn_layer_8')
     fc_l8 = tf.nn.dropout(fc_l8, prob_keep, name='dropout_fc_layer8')
 
 with tf.name_scope("full_connection_layer_9") as scope:
     #W_fc_l9 = tf.Variable(tf.random_uniform([256, 10], -1.0, 1.0))
-    #b_fc_l9 = tf.Variable(tf.random_uniform([1, 10], -1.0, 1.0))
+##    b_fc_l9 = tf.Variable(tf.random_uniform([1, 10], -1.0, 1.0))
+    b_fc_l9 = tf.Variable(tf.random_normal([1, 10]))
     W_fc_l9 = tf.get_variable("W_fc_l9", shape=[256, 10], initializer=xavier_init(256, 10))
-    b_fc_l9 = tf.get_variable("b_fc_l9", shape=[1, 10], initializer=xavier_init(10, 10))
+##    b_fc_l9 = tf.get_variable("b_fc_l9", shape=[1, 10], initializer=xavier_init(10, 10))
     
     fc_l9 = tf.matmul(fc_l8, W_fc_l9) + b_fc_l9
     hypo = tf.nn.softmax(fc_l9, name='hypothesis-fc_layer_9')
@@ -124,7 +134,7 @@ with tf.name_scope("cost_function") as scope:
     cost_summ = tf.summary.scalar("cost", cost)
 
 with tf.name_scope("optimizer") as scope:
-    optimizer = tf.train.AdamOptimizer(0.01).minimize(cost)
+    optimizer = tf.train.AdamOptimizer(0.1).minimize(cost)
 
 validation = tf.equal(tf.arg_max(hypo, 1), tf.arg_max(Y, 1))
 accuracy = tf.reduce_mean(tf.cast(validation, tf.float32))
@@ -139,7 +149,7 @@ writer = tf.summary.FileWriter("./logs/Lab_11_CNN_MNIST_deeper", sess.graph)
 
 start_time = time.time()
 
-for step in range(0, 2001):
+for step in range(0, 1001):
 
     batch = mnist.train.next_batch(50)
 
