b0085647f95793c8172875f9e55beaecdba98a2b
sgkim <sgkim08@gmail.com>
2017-06-13 11:02:45 +0000

    R-pi3, June, 13, 2017

--- rnn_hello/rnn_hello.py
+++ rnn_hello/rnn_hello.py
@@ -11,18 +11,59 @@ x_data = np.array([[1, 0, 0, 0],                        #   h
 
 sample = [char_dic[c] for c in 'hello']
 
-Wi = tf.Variable(tf.random_normal([4, 32]))
-i = tf.placeholder(tf.float32, shape=[None, 4])
+#######################################################################
+##  Shared weights
+#######################################################################
+
+Wi = tf.Variable(tf.random_normal([1, 32]))
 bi = tf.Variable(tf.random_normal([32]))
 
-input_gate = tf.nn.sigmoid(tf.matmul(i, Wi) + bi)
-
 Wf = tf.Variable(tf.random_normal([32, 32]))
-y = tf.Variable(tf.zeros([32, 32]))
 bf = tf.Variable(tf.random_normal([32]))
 
-forget_gate = tf.nn.sigmoid(tf.matmul(y, Wf) + bf)
+Wz = tf.Variable(tf.random_normal([32, 4]))
+bz = tf.Variable(tf.random_normal([4]))
 
-update_y = tf.assign(y, forget_gate)
+#######################################################################
+##  BPTT t0
+#######################################################################
+
+i_0 = tf.placeholder(tf.float32, shape=[None, 1])
+
+y_0 = tf.nn.sigmoid(tf.matmul(i_0, Wi) + bi)        ##  input gate
+
+z_0 = tf.nn.softmax(tf.matmul(y_0, Wz) + bz)        ##  output gate
+
+#######################################################################
+##  BPTT t1
+#######################################################################
+
+i_1 = tf.placeholder(tf.float32, shape=[None, 1])
+
+y_1 = tf.nn.sigmoid(tf.matmul(i_1, Wi) + bi) + tf.nn.sigmoid((y_0, Wf) + bf)        ##  input gate & output gate
+
+z_1 = tf.nn.softmax(tf.matmul(y_1, Wz) + bz)
+
+#######################################################################
+##  BPTT t2
+#######################################################################
+
+i_2 = tf.placeholder(tf.float32, shape=[None, 1])
+
+y_2 = tf.nn.sigmoid(tf.matmul(i_2, Wi) + bi) + tf.nn.sigmoid((y_1, Wf) + bf)
+
+z_2 = tf.nn.softmax(tf.matmul(y_2, Wz) + bz)
+
+#######################################################################
+#   BPTT t3
+#######################################################################
+
+i_3 = tf.placeholder(tf.float32, shape=[None, 1])
+
+y_3 = tf.nn.sigmoid(tf.matmul(i_3, Wi) + bi) + tf.nn.sigmoid((y_2, Wf) + bf)
+
+z_3 = tf.nn.softmax(tf.matmul(y_3, Wz) + bz)
+
+    
 
 
