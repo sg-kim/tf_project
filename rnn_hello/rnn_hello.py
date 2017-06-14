35965ac9ae3732938282a25fc0f05e771d222adb
sgkim <sgkim08@gmail.com>
2017-06-14 23:14:32 +0900

    Desktop, June 14, 2017

--- rnn_hello/rnn_hello.py
+++ rnn_hello/rnn_hello.py
@@ -12,6 +12,20 @@ x_data = np.array([[1, 0, 0, 0],                        #   h
 sample = [char_dic[c] for c in 'hello']
 
 #######################################################################
+##  Inputs
+#######################################################################
+
+i_0 = tf.placeholder(tf.float32, shape=[None, 1])
+i_1 = tf.placeholder(tf.float32, shape=[None, 1])
+i_2 = tf.placeholder(tf.float32, shape=[None, 1])
+i_3 = tf.placeholder(tf.float32, shape=[None, 1])
+
+z_0_ = tf.placeholder(tf.float32, shape=[None, 4])
+z_1_ = tf.placeholder(tf.float32, shape=[None, 4])
+z_2_ = tf.placeholder(tf.float32, shape=[None, 4])
+z_3_ = tf.placeholder(tf.float32, shape=[None, 4])
+
+#######################################################################
 ##  Shared weights
 #######################################################################
 
@@ -28,8 +42,6 @@ bz = tf.Variable(tf.random_normal([4]))
 ##  BPTT t0
 #######################################################################
 
-i_0 = tf.placeholder(tf.float32, shape=[None, 1])
-
 y_0 = tf.nn.sigmoid(tf.matmul(i_0, Wi) + bi)        ##  input gate
 
 z_0 = tf.nn.softmax(tf.matmul(y_0, Wz) + bz)        ##  output gate
@@ -37,8 +49,6 @@ z_0 = tf.nn.softmax(tf.matmul(y_0, Wz) + bz)        ##  output gate
 #######################################################################
 ##  BPTT t1
 #######################################################################
-
-i_1 = tf.placeholder(tf.float32, shape=[None, 1])
 
 y_1 = tf.nn.sigmoid(tf.matmul(i_1, Wi) + bi) + tf.nn.sigmoid((y_0, Wf) + bf)        ##  input gate & output gate
 
@@ -48,8 +58,6 @@ z_1 = tf.nn.softmax(tf.matmul(y_1, Wz) + bz)
 ##  BPTT t2
 #######################################################################
 
-i_2 = tf.placeholder(tf.float32, shape=[None, 1])
-
 y_2 = tf.nn.sigmoid(tf.matmul(i_2, Wi) + bi) + tf.nn.sigmoid((y_1, Wf) + bf)
 
 z_2 = tf.nn.softmax(tf.matmul(y_2, Wz) + bz)
@@ -58,12 +66,17 @@ z_2 = tf.nn.softmax(tf.matmul(y_2, Wz) + bz)
 #   BPTT t3
 #######################################################################
 
-i_3 = tf.placeholder(tf.float32, shape=[None, 1])
-
 y_3 = tf.nn.sigmoid(tf.matmul(i_3, Wi) + bi) + tf.nn.sigmoid((y_2, Wf) + bf)
 
 z_3 = tf.nn.softmax(tf.matmul(y_3, Wz) + bz)
 
-    
+#######################################################################
+#   Training
+#######################################################################
+
+x_entropy_z_0 = tf.nn.softmax_cross_entropy_with_logits(labels=x_data[0], z_0)
+x_entropy_z_1 = tf.nn.softmax_cross_entropy_with_logits(labels=x_data[1], z_1)
+x_entropy_z_2 = tf.nn.softmax_cross_entropy_with_logits(labels=x_data[2], z_2)
+x_entropy_z_3 = tf.nn.softmax_cross_entropy_with_logits(labels=x_data[3], z_3)
 
 
